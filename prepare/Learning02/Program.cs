0efedf13d41b08e7a4a8813b06e7d5ff37d2722c
cross1517 <cashross2rnsy@gmail.com>
2024-09-22 22:39:37 -0600

    week 2 learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Horse Hugger";
+        job1._company = "Horse Hugs Inc.";
+        job1._startYear = 2010;
+        job1._endYear = 2012;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Horse Whisperer";
+        job2._company = "Horse Whispers Inc.";
+        job2._startYear = 2012;
+        job2._endYear = 2014;
+
+        //job1.DisplayJob();
+        //job2.DisplayJob();
+
+        Resume resume = new Resume();
+        resume._name = "Logan McCaul";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
